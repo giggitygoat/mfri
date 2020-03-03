917cb7822c56d97182a71bdf27059f43687fb5af
root <root@vmi335798.contaboserver.net>
2020-03-03 14:47:32 +0100

    automatic message

--- mfri/recipes/views.py
+++ mfri/recipes/views.py
@@ -228,7 +228,7 @@ def scrapeMumum(request):
         antalSider = 156
         url= 'https://dr.dk/mad/opskrift/'
         #heleMummum(url)
-        for i in range(60, 75):
+        for i in range(75,90):
             stringBuilder = url+"?page="+str(i)
             print(stringBuilder)
             heleMummum(stringBuilder)
