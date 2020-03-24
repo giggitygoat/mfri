37da7f11b05edeb298491790a30197684f123f55
root <root@vmi335798.contaboserver.net>
2020-03-24 13:44:10 +0100

    automatic message

--- mfri/mfri/urls.py
+++ mfri/mfri/urls.py
@@ -47,6 +47,7 @@ def p4Alarm(request):
     if request.method == 'POST':
         if 'file' in request.FILES:
             file = request.FILES['file']
+            print(file)
             destination = open('/home/smadrekasse/filename.jpg', 'wb')
             for chunk in file.chunks():
                 destination.write(chunk)
