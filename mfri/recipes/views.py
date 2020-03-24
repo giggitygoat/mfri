01920f776b9c3ecc10e6afc72463db1db3997e70
root <root@vmi335798.contaboserver.net>
2020-03-24 01:37:34 +0100

    automatic message

--- mfri/recipes/views.py
+++ mfri/recipes/views.py
@@ -43,7 +43,7 @@ def send_to_token(msg):
 
     # See documentation on defining a message payload.
     noti = messaging.AndroidNotification(
-        sound='res_raw_fire_pager',
+        sound='res/raw/fire_pager',
         body=msg,
         title='titel',
 
