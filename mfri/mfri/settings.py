61378d6d1067479efa58a15f1a1cbcda83ca0171
root <root@vmi335798.contaboserver.net>
2020-05-14 22:24:53 +0200

    automatic message

--- mfri/mfri/settings.py
+++ mfri/mfri/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'after_response',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
