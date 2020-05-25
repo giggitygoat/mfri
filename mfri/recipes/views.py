154cb63d67e960dbf9ca0935b2aee68195ace6f3
root <root@vmi335798.contaboserver.net>
2020-05-25 12:39:49 +0200

    automatic message

--- mfri/recipes/views.py
+++ mfri/recipes/views.py
@@ -82,7 +82,7 @@ def send_to_token(msg):
     # Response is a message ID string.
     for i in response.responses:
         print('Respnse code for application nr: ',i, '= ',i.messageId)
-    print('Response code from Firebase: :', response.responses)
+    #print('Response code from Firebase: :', response.responses)
     # [END send_to_token]
 
 def scraperMain(url):
